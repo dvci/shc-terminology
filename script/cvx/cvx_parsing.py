088cca77d723ab93646b902d1cd8c2671e95fc26
Max Masnick <max@masnick.org>
2022-09-01 12:45:36 -0400

    Improve CVX parsing and update CVX value set

--- script/cvx/cvx_parsing.py
+++ script/cvx/cvx_parsing.py
@@ -1,8 +1,11 @@
 import json
-import re
-import requests
+import logging
 import os
-from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
+import requests
+import yaml
 
 
 def retrieve_new_cvx():
@@ -13,36 +16,38 @@ def retrieve_new_cvx():
     CVX = 'https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/cvx.txt'
 
     # retrieve flat file data
-    response = requests.get(CVX)
+    response = requests.get(CVX, verify=False)
 
     response.encoding = 'utf-8'
 
     with open('cvx_cdc.txt', 'w') as outfile:
         outfile.write(response.text)
 
-    with open('cvx_cdc.txt', 'r') as readfile:
-        lines = readfile.readlines()
-        cvx_dict = {}
-        for line in lines:
-            sub_dict = {}
-            split_list = line.split('|')
-            cvx_dict[split_list[0].strip()] = sub_dict
-            sub_dict['short-description'] = split_list[1]
-            sub_dict['full-vaccine-name'] = split_list[2]
-            sub_dict['vaccine-status'] = split_list[4]
-            sub_dict['last-update-date'] = split_list[6]
-            sub_dict['notes'] = split_list[3]
-
-    # # convert keys to integers for ordering
-    # cvx_dict = {int(key): value for key, value in cvx_dict.items()}
-
-    # # reorder dictionary by increasing value of keys
-    # cvx_dict = OrderedDict(sorted(cvx_dict.items()))
-
-    return cvx_dict
+    # Convert .txt file to dict where keys are CVX codes and values are a dict of metadata for that code
+    df = pd.read_csv('cvx_cdc.txt', sep='|', names=[
+        'short-description',
+        'full-vaccine-name',
+        'notes',
+        'vaccine-status',
+        'nonvaccine',
+        'last-update-date',
+    ])
+    return df.to_dict(orient='index')
 
 
-def retrieve_new_name():
+def filter_cvx(new_cvx, terms):
+    out = {}
+
+    for cvx, data in new_cvx.items():
+        for term in terms:
+            if (term.lower() in data['short-description'].lower()) or (term.lower() in data['full-vaccine-name'].lower()):
+                out[cvx] = data
+                break
+
+    return out
+
+
+def retrieve_new_cvx_product_names():
     '''
     use requests.get to retrieve latest trade name flat file from CDC
     saves the file and returns for reading
@@ -50,179 +55,177 @@ def retrieve_new_name():
     PRODUCT = 'https://www2a.cdc.gov/vaccines/iis/iisstandards/downloads/TRADENAME.txt'
 
     # retrieve flat file data
-    response = requests.get(PRODUCT)
-    
+    response = requests.get(PRODUCT, verify=False)
+
     # extract content and write to a file
     with open('name_cdc.txt', 'w') as outfile:
         outfile.write(response.text)
 
-    # open file to read for processing
-    with open('name_cdc.txt', 'r') as readfile:
-        lines = readfile.readlines()
-        name_dict = {}
-        for line in lines[1:]:
-            sub_dict = {}
-            split_list = line.split('|')
-            name_dict[split_list[2].strip()] = sub_dict
-            sub_dict['prod_name'] = split_list[0]
-            sub_dict['Description'] = split_list[1]
+    df = pd.read_csv('name_cdc.txt', sep='|', index_col=False, names=[
+        'prod-name',
+        'short-description',
+        'cvx-code',
+        'manufacturer-name',
+        'mvx-code',
+        'mvx-status',
+        'product-name-status',
+        'last-update-date'
+    ])
 
-    return name_dict
+    # Removing filter by disease for now -- can add back in later if necessary
+    # Filter to just diseases of interest
+    # of_interest = ['covid', 'monkeypox', 'smallpox']
 
+    # Find rows that contain the disease name of interest, based on https://stackoverflow.com/a/43018248
+    # filter = np.any([df.apply(lambda r: r.str.contains(s, case=False).any(), axis=1) for s in of_interest], 0)
 
-def retrieve_old_cvx():
-    '''
-    retrieves saved updated cvx file from same folder for comparison
-    returns most up-to-date dictionary of codes
-    '''
-    with open ('cvx_updated.json', 'r', encoding='utf-8') as json_file:
-        data_dict = json.load(json_file)
+    # Only include non-blank product names
+    f = np.all([df['prod-name'].notnull(), (df['prod-name'] != '')], 0)
 
-    return data_dict
+    # Only include active product names
+    df_filtered = df[f & (df['product-name-status'] == 'Active')]
 
-
-def find_diffs(old_cvx, new_cvx):
-
-    old_set = set(old_cvx)
-    new_set = set(new_cvx)
-
-    difference = new_set - old_set
-
-    if len(difference) == 0:
-        return None
-    else:
-        return difference
+    # If there are duplicate CVX x product-name combinations, choose the one with the most recent `last-update-date`
+    # Then spit out a dict where the key is the CVX code and the value is the product name.
+    return df_filtered \
+        .sort_values(by=['cvx-code', 'last-update-date']) \
+        .drop_duplicates(subset=['cvx-code'], keep='last', ignore_index=True) \
+        .set_index('cvx-code')['prod-name'].to_dict()
 
 
-def update_cvx(old_cvx, new_cvx, name_dict):
-    '''
-    compares the old and new cvx files 
-    updates information for matching keys in the old file
-    retains old codes and adds new one's
-    '''
-    old_cvx.update(new_cvx)
+def read_value_set(filename):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, f'../../input/vocabulary/{filename}')
 
-    for k, v in name_dict.items():
-        if k in old_cvx:
-            old_cvx[k].update(v)
-
-    with open('updated_cvx.txt', 'w') as file:
-        file.write(json.dumps(old_cvx))
-
-    return old_cvx
+    with open(filename, 'r') as f:
+        return json.load(f)
 
 
-def covid_dict(updated_dict):
-    '''
-    filters the updated dictionary to just
-    key, value pairs that are COVID-19 vaccines
-    returns the filtered COVID-19 dictionary
-    '''
-    covid_dict = {}
-    
-    for key, value in updated_dict.items():
-        for k, v in value.items():
-            if 'COVID-19' in v:
-                covid_dict[key] = value
+def write_value_set(filename, value_set):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, f'../../input/vocabulary/{filename}')
 
-    return covid_dict
+    with open(filename, 'w') as f:
+        f.write(json.dumps(value_set, indent=2))
 
 
-def update_value_set(covid_dict, target_dir):
+def read_excluded_cvx():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'cvx_exclude.yaml')
+
+    with open(filename, "r") as f:
+        return yaml.safe_load(f)
+
+
+def cvx_product_name(cvx_code, new_cvx, cvx_product_names):
+    return cvx_product_names[cvx_code] if cvx_code in cvx_product_names else new_cvx[cvx_code][
+        'short-description']
+
+
+def update_value_set(value_set, new_cvx, cvx_product_names):
     '''
     update the covid-cvx.json vocab file with all
     COVID-19 vaccines
     '''
 
-    json_valuset = open(target_dir+'/input/vocabulary/covid-cvx.json', 'r')
+    # CVX codes are strings in the ValueSet, so make sure we are working with strings everywhere
+    new_cvx = {str(k): v for k, v in new_cvx.items()}
+    cvx_product_names = {str(k): v for k, v in cvx_product_names.items()}
 
-    vs = json.load(json_valuset)
+    # Remove manually excluded CVX
+    excluded = read_excluded_cvx()
+    logging.info(f'{len(excluded)} CVX code(s) manually excluded in cvx_exclude.yaml')
+    logging.info(f'{len(new_cvx)} CVX code(s) before applying exclusion')
+    new_cvx = {k: v for k, v in new_cvx.items() if k not in excluded}
+    logging.info(f'{len(new_cvx)} CVX code(s) after applying exclusion')
 
-    # default designation dictionary that is sub-dictionary in 
-    # in the vocab file
-    designation_dict = [
-      {
-        "language": "en-US",
-        "use": {
-          "system": "https://terminology.smarthealth.cards/CodeSystem/designation-use",
-          "use": "consumer-friendly"
-          },
-          "value": ""
-          }
-    ] 
+    # Pull out CVX codes already in the ValueSet for convenience
+    existing_concepts = value_set['compose']['include'][0]['concept']
+    current_cvx_codes = [str(c['code']) for c in existing_concepts]
 
-    # check that all the cvx codes are integers
-    for code in vs['compose']['include'][0]['concept']:
-        code['code'] = int(code['code'])
-    
-    # access the list of code dictionaries within value set file
-    code_subdict = vs['compose']['include'][0]['concept']
-    current_cvx_codes = []
+    # Find CVX codes in the `cvx` dict that are already in the value set
+    cvx_already_included = [c for c in new_cvx.keys() if c in current_cvx_codes]
 
-    # make reference list of cvx codes that are already in the vocab file
-    for code in code_subdict:
-        current_cvx_codes.append(code['code'])
+    # Find CVX codes that are in the `cvx` dict and are **brand new** to the value set
+    cvx_to_add = [c for c in new_cvx.keys() if c not in cvx_already_included]
 
-    # iterate through the covid dictionary
-    for key, value in covid_dict.items():
-        # if the key is already present, pass
-        if int(key) in current_cvx_codes:
-            pass
-        # otherwise, build a new dictionary for the new cvx code in the format of the vocab file
-        else:
-            one_code = {}
-            one_code['code'] = int(key)
-            one_code['display'] = value['short-description']
-            # it's possible the cvx file won't have a prod_name so if else statement to check if it exists
-            if 'prod_name' in value.keys():
-                designation_dict[0]['value'] = value['prod_name']
-            else:
-                designation_dict[0]['value'] = ""
-            one_code['designation'] = designation_dict
-            vs['compose']['include'][0]['concept'].append(one_code)
-    
-    # order the list of dictionaries by increasing cvx value 
-    ordered_vs = sorted(vs['compose']['include'][0]['concept'], key=lambda d: d['code'])
+    # Logging
+    logging.info(f'{len(current_cvx_codes)} CVX code(s) currently in the value set')
+    logging.info(f'{len(new_cvx)} CVX code(s) in the canonical list of CVX for inclusion, {len(cvx_already_included)} of which are already in the value set')
+    logging.info(f'{len(cvx_to_add)} CVX code(s) to be added to the value set')
 
-    # insert sorted value set back into json value set
-    vs['compose']['include'][0]['concept'] = ordered_vs
+    # Add the new CVX codes to the value set
+    for cvx_code in cvx_to_add:
+        product_name = cvx_product_name(cvx_code, new_cvx, cvx_product_names)
+        logging.info(f'  Adding CVX {cvx_code} <{product_name}> to the value set')
+        value_set['compose']['include'][0]['concept'].append(
+            {
+                "code": str(cvx_code),
+                "display": new_cvx[cvx_code]['short-description'],
+                "designation": [
+                    {
+                        "language": "en-US",
+                        "use": {
+                            "system": "https://terminology.smarthealth.cards/CodeSystem/designation-use",
+                            "use": "consumer-friendly"
+                        },
+                        "value": product_name
+                    }
+                ]
+            }
+        )
 
-    return vs
+    # Sort the `concept` contents by `code`
+    value_set['compose']['include'][0]['concept'] = sorted(
+        value_set['compose']['include'][0]['concept'],
+        key=lambda x: x['code']
+    )
+
+    # Update display of existing CVX codes -- this should always be the official short display of the code system
+    for cvx_code in cvx_already_included:
+        # `i` is the index of this CVX code within the concept element
+        i, existing_concept = [(i, c) for i, c in enumerate(value_set['compose']['include'][0]['concept']) if c['code'] == cvx_code][0]
+        value_set['compose']['include'][0]['concept'][i]['display'] = new_cvx[cvx_code]['short-description']
+
+        # If a `en-US` designation is missing, add one
+        if ('designation' not in (value_set['compose']['include'][0]['concept'][i])):
+            value_set['compose']['include'][0]['concept'][i]['designation'] = []
+
+        if not [d for d in value_set['compose']['include'][0]['concept'][i]['designation'] if d['language'] == 'en-US']:
+            value_set['compose']['include'][0]['concept'][i]['designation'].append({
+                {
+                    "language": "en-US",
+                    "use": {
+                        "system": "https://terminology.smarthealth.cards/CodeSystem/designation-use",
+                        "use": "consumer-friendly"
+                    },
+                    "value": cvx_product_name(cvx_code, new_cvx, cvx_product_names)
+                }
+            })
+
+    return value_set
 
 
 def main():
+    logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
-    # retrieve newest CVX file from CDC
+    COVID_VALUE_SET = 'covid-cvx.json'
+
+    # Retrieve newest CVX file from CDC
     new_cvx = retrieve_new_cvx()
 
-    # retrieve newest Manufacturer name file from CDC
-    new_name = retrieve_new_name()
+    # Filter to just COVID-related
+    new_cvx = filter_cvx(new_cvx, ['covid'])
 
-    # retrieve old CVX file from previous run
-    old = retrieve_old_cvx()
+    # Retrieve newest product name file from CDC
+    cvx_product_names = retrieve_new_cvx_product_names()
 
-    # update the CVX dictionary
-    updated_dict = update_cvx(old, new_cvx, new_name)
+    # Load in existing value set
+    value_set = read_value_set(COVID_VALUE_SET)
 
-    # save the updated file for reuse in next update
-    with open('cvx_updated.json','w') as outfile:
-            json.dump(updated_dict, outfile, indent=4)
+    # Update value set
+    value_set = update_value_set(value_set, new_cvx, cvx_product_names)
+    write_value_set(COVID_VALUE_SET, value_set)
 
-    covid = covid_dict(updated_dict)
-
-    # get current file path    
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    
-    # move two levels up in directory to get value set file
-    target_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-2])
-
-    # update the value set
-    updated_vs = update_value_set(covid, target_dir)
-
-    # save the updated file for reuse in next update
-    with open(target_dir+'/input/vocabulary/covid-cvx.json','w') as outfile:
-            json.dump(updated_vs, outfile, indent=4)
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- script/cvx/test_cvx_parsing.py
+++ script/cvx/test_cvx_parsing.py
@@ -1,10 +1,22 @@
-import unittest
-from cvx_parsing import retrieve_new_cvx
+import pytest
+from cvx_parsing import *
 
-class TestCVXParsing(unittest.TestCase):
 
-    def test_retrieve_new_cvx(self):
+def test_retrieve_new_cvx():
+    cvx = retrieve_new_cvx()
+    assert cvx[208]['short-description'] == "COVID-19, mRNA, LNP-S, PF, 30 mcg/0.3 mL dose"
 
-        actual = retrieve_new_cvx()
-        self.assertEqual(actual['208']['short-description'], "COVID-19, mRNA, LNP-S, PF, 30 mcg/0.3 mL dose")
-    
+
+def test_retrieve_new_cvx_product_names():
+    names = retrieve_new_cvx_product_names()
+    assert names[208] == 'COMIRNATY'
+
+    # No empty product names
+    assert [n for n in names.values() if n == ''] == []
+
+
+def test_filter_cvx():
+    cvx = retrieve_new_cvx()
+
+    filter1 = filter_cvx(cvx, ['smallpox', 'monkeypox'])
+    assert list(filter1.keys()) == [75, 105, 206]
